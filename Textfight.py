ff27e0add82d2cd778db18f987a70da856f87e76
viggope <59804534+viggope@users.noreply.github.com>
2021-07-07 20:20:25 +0200

    Update Textfight.py

--- Textfight.py
+++ Textfight.py
@@ -3,10 +3,13 @@ import random
 
 inv = []
 weapons = ['Fist']
+armors = []
 stageID: str = '0'
 game = 1
 health = 100
-
+armor = 0
+window1 = 'notbroken'
+key1 = False
 def fight(phealth, enemy, ehealth, eweapon1, protectRate=3, stage=None, protectionAmount=None, edamage1=None, edamage2=None, edamage3=None, eweapon2=None, eweapon3=None):
     playerHealth = phealth
     fighting = 1
@@ -33,6 +36,7 @@ def fight(phealth, enemy, ehealth, eweapon1, protectRate=3, stage=None, protecti
                 time.sleep(2)
                 if ehealth <= 0:
                     print(enemy + ' died')
+                    stage[len(stage)] = 'b'
             if do == '2':
                 if weapons[0] == 'stick':
                     playerHealth += 10
@@ -45,7 +49,10 @@ def fight(phealth, enemy, ehealth, eweapon1, protectRate=3, stage=None, protecti
                 time.sleep(2)
             if do == '3':
                 print('You escapes with ' + str(playerHealth) + ' health')
-                stage = '2'
+                stagepart = stage[0:len(stage) - 1]
+                int(stagepart)
+                stage = stagepart
+
 
 
 
@@ -101,17 +108,6 @@ def fight(phealth, enemy, ehealth, eweapon1, protectRate=3, stage=None, protecti
             return stage
 
 
-
-
-
-
-
-
-
-
-
-
-
 while game == 1:
     if stageID == '0':
         print('You wakes up in a forest...')
@@ -153,7 +149,7 @@ while game == 1:
         print('In front of you, a cave appears')
 
     if stageID == '3':
-        print('In front of you, a cave appears')
+        print('A lot of meters away a house appears')
     time.sleep(1)
 
     while stageID == '2':
@@ -161,14 +157,163 @@ while game == 1:
         if do == '1':
             print('You goes into the cave and you see a BEAR!')
             stageID = '2A'
-            health = 100
+            health = 100 + armor
         if do == '2':
             stageID = '1'
         else:
             pass
     while stageID == '2A':
-        stageID = fight(phealth=health, enemy='Bear', eweapon1='Paw', eweapon2='Second Paw',  ehealth=150, edamage1=25, edamage2=25)
+        stageID = fight(phealth=health, enemy='Bear', eweapon1='Paw', eweapon2='Second Paw',  ehealth=150, edamage1=25, edamage2=25, stage=stageID)
+        print(': - Bear defeated - :')
+        #time.sleep(1)
+        #print('Bear dropped | Bear armor |')
+        #armors.insert(0, 'bear armor')
+        #inv.insert(0, 'bear armor')
+        #armor = 25
 
+    while stageID == '3':
+        do: str = input('1 Go to house \n2 Turn back\n')
+        if do == '1':
+            print('You goes to the house...')
+            stageID = '3A'
+        if do == '2':
+            stageID = '1'
+        else:
+            pass
+
+    while stageID == '3A':
+        if weapons[0] == 'stick':
+            if window1 == 'notbroken':
+                do: str = input('1 Knock on the door\n2 Break window with stick\n3 Turn back\n')
+                if do == '1':
+                    print('You knocks on the door...')
+                    time.sleep(2)
+                    print('...')
+                    time.sleep(4)
+                    print('No answer')
+                    pass
+                if do == '2':
+                    stageID = '3B'
+                    
+                if do == '3':
+                    stageID = '3'
+                else:
+                    pass
+                
+            else:
+                do: str = input('1 Knock on the door\n2 Climb into crushed window\n3 Turn back\n')
+                if do == '1':
+                    print('You knocks on the door...')
+                    time.sleep(2)
+                    print('...')
+                    time.sleep(4)
+                    print('No answer')
+                    pass
+                if do == '2':
+                    stageID = '3D'
+
+                if do == '3':
+                    stageID = '3'
+                else:
+                    pass
+        else:   
+            do: str = input('1 Knock \n2 Turn back\n')
+        if do == '1':
+            print('You knocks on the door...')
+            time.sleep(2)
+            print('...')
+            time.sleep(4)
+            print('No answer')
+            pass
+        if do == '2' and not weapons[0] == 'Fist':
+            stageID = '3B'
+        if do == '3' and not weapons[0] == 'Fist':
+            stageID = '3'
+        elif do == '2' and weapons[0] == 'Fist':
+            stageID = '3'
+        else:
+            pass
+    
+    while stageID == '3B':
+        window1 = 'broken'
+        do: str = input('1 Climb into crushed window\n2 Run away\n')
+        if do == '1':
+            print('You knocks on the door...')
+        
+        if do == '1':
+            stageID = '3C'
+    
+    while stageID == '3C': #needs to own stick or a breaker tool
+        
+        do: str = input('1 Knock on the door\n2 Climb into window\n3 Turn back\n')
+
+        if do == '1':
+            print('You knocks on the door...')
+            time.sleep(2)
+            print('...')
+            time.sleep(4)
+            print('No answer')
+            pass
+        if do == '2':
+            print('You climbs into the broken window...')
+            time.sleep(1)
+            print('In the house, there is a key on a table')
+            stageID = '3D'
+        if do == '3':
+            stageID = '3'
+        else:
+            pass
+
+    while stageID == '3D':  # needs to own stick or a breaker tool
+
+        do: str = input('1 Pick key\n2 Keep going\n3 Turn back\n')
+
+        if do == '1':
+            print('You Picks up the key and keeps going around in the house')
+            key1 = True
+            time.sleep(1)
+            print('You found a fridge, a door and a bed')
+            
+            time.sleep(2)
+            stageID = '3E'
+            
+        if do == '2':
+            stageID = '3E'
+        if do == '3':
+            stageID = '3'
+        else:
+            pass
+
+    while stageID == '3E':  # needs to own stick or a breaker tool
+
+        do: str = input('1 Open fridge\n2 Open door\n3 Check bed\n4 Turn back\n')
+
+        if do == '1':
+            print('You opens the fridge...')
+            time.sleep(2)
+            print('The fridge is empty')
+           
+            time.sleep(2)
+            pass
+        if do == '2':
+            time.sleep(1)
+            if key1 == True:
+                print('You opens the door with the key...')
+                stageID = '3F'
+                
+            if key1 == False:
+                print('The door is locked')
+                pass
+        
+        if do == '3':
+            print('You looks under the bed but there is nothing there')
+            pass
+            
+        
+        if do == '4':
+            stageID = '3'
+        else:
+            pass
 
 
 
